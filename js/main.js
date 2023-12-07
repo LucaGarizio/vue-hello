2cb788d22595a48bf4711f5b2e8ed3bc74794167
Luca Garizio <garizioluca2@gmail.com>
2023-12-07 14:13:30 +0100

    fix: add comment on js

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ createApp({
     // messaggio iniziale e classe 
       message: 'Vue JS è bellissimo0o0o0o0o0o!!!!111!!!',
       titleClass: "title",
-    //   classe contenitore e immagine relativa
+    // classe contenitore e immagine relativa
       contImg: "class-cont-img",
       foto: "wallpaperflare.com_wallpaper(4).jpg",
       imgClass: "img"
     }
   },
-// bonus
+// bonus con lifecycle hooks (mounted)
+// esegue la funzione dopo che tutti i componenti iniziali sono stati caricati
   mounted() {
+    // mostra l'immagine dopo 3 secondi
     setTimeout(() => {
         this.contImg = "show";
     },3000);
